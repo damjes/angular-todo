7105e462ba3fc6755ecb65916593f70627aa74d1
Damjes <git@damj.es>
2025-11-10 13:20:49 +0100

    Poprawienie spacji na taby

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.sass'
+	selector: 'app-root',
+	imports: [RouterOutlet],
+	templateUrl: './app.html',
+	styleUrl: './app.sass',
 })
 export class App {
-  protected readonly title = signal('angular-todo');
+	protected readonly title = signal('angular-todo');
 }
